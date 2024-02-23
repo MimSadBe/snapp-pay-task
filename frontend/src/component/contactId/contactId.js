75d243e32ecb63c3d30672fa067105fbe9d132f8
Mohammad Sadegh Babaei <mimsadbe@gmail.com>
2024-02-23 19:39:50 +0330

    fix: Refactor and complete add to local storage function .

--- frontend/src/component/contactId/contactId.js
+++ frontend/src/component/contactId/contactId.js
@@ -2,6 +2,7 @@ import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import Modal from "../modal/modal";
 
+
 const ContactId = () => {
     const {id} = useParams();
     const [contact, setContact] = useState(null)
@@ -36,173 +37,180 @@ const ContactId = () => {
         }
     }
 
-    useEffect(() => {
-        getData()
-    }, [id]);
-
     const addToVisit = (contact) => {
-        let lastVisitContact = localStorage.getItem("lastVisitContact");
-
+        const lastVisitContact = localStorage.getItem("lastVisitContact");
+        let arrayContact = [];
         if (lastVisitContact) {
-            lastVisitContact = JSON.parse(lastVisitContact);
-            if (Number(lastVisitContact.totalVisit) > 0) {
-                lastVisitContact.totalVisit = +lastVisitContact.totalVisit + 1
+            const parsLastVisitContact = JSON.parse(lastVisitContact);
+            let thisContact = parsLastVisitContact.find((item) => +item.id === +id)
+            if (thisContact) {
+                thisContact.totalVisit = +thisContact.totalVisit + 1
+                arrayContact = [...parsLastVisitContact]
             } else {
-                lastVisitContact.totalVisit = 1;
-
+                let _contact = {
+                    ...contact,
+                    "totalVisit": 1
+                }
+                arrayContact = [...parsLastVisitContact, _contact]
             }
-            localStorage.setItem("lastVisitContact", JSON.stringify(lastVisitContact))
+            if (parsLastVisitContact.length > 4) {
+                arrayContact.shift()
+            }
         } else {
             let _contact = {
                 ...contact,
                 "totalVisit": 1
             }
-            localStorage.setItem("lastVisitContact", JSON.stringify(_contact))
+            arrayContact.push(_contact)
         }
+        localStorage.setItem("lastVisitContact", JSON.stringify(arrayContact))
     }
 
+    useEffect(() => {
+        getData()
+    }, [id]);
 
     return (
-        <div className="">
+        
+        <div className="bg-background-id h-screen container relative py-3 flex flex-col max-h-screen overflow-y-auto">
+
+            <header className="flex justify-between items-center mb-4">
+                <Link to="/" className="text-main-color flex items-center font-semibold">
+                    <img src="/assets/img/blue_left_arrow.svg" alt="Arrow left"/>
+                    Contacts
+                </Link>
+                <span className="text-main-color cursor-no-drop font-semibold">Edit</span>
+            </header>
+
             <div
-                className="bg-background-id h-screen container relative py-3 flex flex-col max-h-screen overflow-y-auto">
-
-                <header className="flex justify-between items-center mb-4">
-                    <Link to={"/"} className="text-main-color flex items-center font-semibold">
-                        <img src="/assets/img/blue_left_arrow.svg" alt="Arrow left"/>
-                        Contacts
-                    </Link>
-                    <span className="text-main-color cursor-no-drop font-semibold">Edit</span>
-                </header>
-
-                <div
-                    className={"size-24 rounded-full bg-gradient-to-b from-[#a2a9b1] to-[#878b94] mx-auto flex items-center justify-center text-3xl font-bold text-white mb-2"}>
-                    {
-                        contact?.avatar ?
-                            <img className={"w-full object-contain rounded-full"} src={`${contact?.avatar}`}
-                                 alt={`${contact?.first_name + contact?.last_name}`}/>
-                            :
-                            contact?.first_name?.split("")[0] + " " + contact?.last_name?.split("")[0]
-                    }
-                </div>
-
-                <div className="mb-6">
-                    <div className={"w-full justify-center items-center flex gap-1 font-semibold text-2xl"}>
-                        <span>{contact?.first_name}</span>
-                        <span>{contact?.last_name}</span>
-                    </div>
-                    <div className="w-full justify-center items-center flex text-gray-color">
-                        {contact?.company}
-                    </div>
-                </div>
-
-                <div className="w-100 grid grid-cols-5 gap-2 mb-4">
-                    <div
-                        className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2 text-main-color cursor-pointer relative"
-                        onClick={() => toggleStatusModal("message")}
-                    >
-
-                        <img src="/assets/img/message.svg" alt="message"/>
-                        message
-
-                        <Modal
-                            showStatus={statusModal.message}
-                            phoneNumber={contact?.phone}
-                            type="message"
-                        />
-                    </div>
-                    <div
-                        className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2 text-main-color cursor-pointer relative"
-                        onClick={() => toggleStatusModal("phone")}
-                    >
-
-                        <img src="/assets/img/phone.svg" alt="phone"/>
-                        call
-
-                        <Modal
-                            showStatus={statusModal.phone}
-                            phoneNumber={contact?.phone}
-                            type="phone"
-                            idTelegram={contact?.telegram}
-                        />
-                    </div>
-                    <div
-                        className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2  text-main-color  cursor-pointer relative"
-                        onClick={() => toggleStatusModal("videoCall")}
-                    >
-
-                        <img src="/assets/img/video.svg" alt="video"/>
-                        video
-                        <Modal
-                            showStatus={statusModal.videoCall}
-                            phoneNumber={contact?.phone}
-                            type="videoCall"
-                            idTelegram={contact?.telegram}
-                        />
-                    </div>
-                    <a href={`mailto:${contact?.email}`}
-                       className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2  text-main-color  cursor-pointer relative"
-                    >
-
-                        <img src="/assets/img/mail.svg" alt="mail"/>
-                        mail
-
-                    </a>
-                    <div className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2  text-main-color  cursor-no-drop relative grayscale">
-
-                        <img src="/assets/img/currency-dollar.svg" alt="pay"/>
-                        pay
-
-                    </div>
-                </div>
-
-                {contact?.phone &&
-                    <div className="flex flex-col p-3 rounded-xl bg-white mb-4 cursor-pointer">
-                        <span className="font-semibold">phone</span>
-                        <span className="text-main-color text-md">{contact?.phone}</span>
-                    </div>
+                className="size-24 min-h-24 rounded-full bg-gradient-to-b from-[#a2a9b1] to-[#878b94] mx-auto flex items-center justify-center text-3xl font-bold text-white mb-2">
+                {
+                    contact?.avatar ?
+                        <img className="w-full object-contain rounded-full" src={`${contact?.avatar}`}
+                             alt={`${contact?.first_name + contact?.last_name}`}/>
+                        :
+                        (contact?.first_name && contact?.last_name) && contact?.first_name?.split("")[0] + " " + contact?.last_name?.split("")[0]
                 }
-                {contact?.telegram &&
-                    <div className="flex flex-col p-3 rounded-xl bg-white mb-4 cursor-pointer">
-                        <span className="font-semibold">Telegram</span>
-                        <span className="text-main-color text-md">https://t.me/@{contact?.telegram}</span>
-                    </div>
-                }
+            </div>
 
-                {contact?.note &&
-                    <div className="flex flex-col p-3 rounded-xl bg-white mb-4">
-                        <span className=" font-semibold">note</span>
-                        <span className="text-main-color text-md">{contact?.note}</span>
-                    </div>
-                }
-
-                {contact?.gender &&
-                    <div className="flex flex-col p-3 rounded-xl bg-white mb-4">
-                        <span className=" font-semibold">gender</span>
-                        <span className="text-main-color text-md">{contact?.gender}</span>
-                    </div>
-                }
-
-                <div className="flex flex-col py-3 rounded-xl bg-white mb-4">
-                    <a href={`sms:${contact?.phone}`}
-                       className="text-main-color border-b ml-4 pb-2 mb-2 border-background-id font-semibold">
-                        Send Message
-                    </a>
-                    <div className="text-main-color border-b ml-4 pb-2 mb-2 border-background-id font-semibold">
-                        Share Contact
-                    </div>
-                    <div className="text-main-color ml-4 font-semibold">Add To Favorite</div>
+            <div className="mb-6">
+                <div className="w-full justify-center items-center flex gap-1 font-semibold text-2xl">
+                    <span>{contact?.first_name}</span>
+                    <span>{contact?.last_name}</span>
                 </div>
-
-                <div className="flex flex-col p-3 rounded-xl bg-white mb-4 text-main-color text-md  font-semibold">
-                    Add To Emergency Contact
-                </div>
-
-                <div className="flex flex-col p-3 rounded-xl bg-white mb-4 text-main-color text-md  font-semibold">
-                    Share My Location
+                <div className="w-full justify-center items-center flex text-gray-color">
+                    {contact?.company}
                 </div>
             </div>
+
+            <div className="w-100 grid grid-cols-5 gap-2 mb-4">
+                <div
+                    className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2 text-main-color cursor-pointer relative"
+                    onClick={() => toggleStatusModal("message")}
+                >
+
+                    <img src="/assets/img/message.svg" alt="message"/>
+                    message
+
+                    <Modal
+                        showStatus={statusModal.message}
+                        phoneNumber={contact?.phone}
+                        type="message"
+                    />
+                </div>
+                <div
+                    className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2 text-main-color cursor-pointer relative"
+                    onClick={() => toggleStatusModal("phone")}
+                >
+
+                    <img src="/assets/img/phone.svg" alt="phone"/>
+                    call
+
+                    <Modal
+                        showStatus={statusModal.phone}
+                        phoneNumber={contact?.phone}
+                        type="phone"
+                        idTelegram={contact?.telegram}
+                    />
+                </div>
+                <div
+                    className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2  text-main-color  cursor-pointer relative"
+                    onClick={() => toggleStatusModal("videoCall")}
+                >
+
+                    <img src="/assets/img/video.svg" alt="video"/>
+                    video
+                    <Modal
+                        showStatus={statusModal.videoCall}
+                        phoneNumber={contact?.phone}
+                        type="videoCall"
+                        idTelegram={contact?.telegram}
+                    />
+                </div>
+                <a href={`mailto:${contact?.email}`}
+                   className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2  text-main-color  cursor-pointer relative"
+                >
+
+                    <img src="/assets/img/mail.svg" alt="mail"/>
+                    mail
+
+                </a>
+                <div
+                    className="w-full bg-white rounded-xl flex items-center justify-center flex-col text-sm p-2  text-main-color  cursor-no-drop relative grayscale">
+
+                    <img src="/assets/img/currency-dollar.svg" alt="pay"/>
+                    pay
+
+                </div>
+            </div>
+
+            {contact?.phone &&
+                <div className="flex flex-col p-3 rounded-xl bg-white mb-4 cursor-pointer">
+                    <span className="font-semibold">phone</span>
+                    <span className="text-main-color text-md">{contact?.phone}</span>
+                </div>
+            }
+            {contact?.telegram &&
+                <div className="flex flex-col p-3 rounded-xl bg-white mb-4 cursor-pointer">
+                    <span className="font-semibold">Telegram</span>
+                    <span className="text-main-color text-md">https://t.me/@{contact?.telegram}</span>
+                </div>
+            }
+
+            {contact?.note &&
+                <div className="flex flex-col p-3 rounded-xl bg-white mb-4">
+                    <span className="font-semibold">note</span>
+                    <span className="text-main-color text-md">{contact?.note}</span>
+                </div>
+            }
+
+            {contact?.gender &&
+                <div className="flex flex-col p-3 rounded-xl bg-white mb-4">
+                    <span className="font-semibold">gender</span>
+                    <span className="text-main-color text-md">{contact?.gender}</span>
+                </div>
+            }
+
+            <div className="flex flex-col py-3 rounded-xl bg-white mb-4">
+                <a href={`sms:${contact?.phone}`}
+                   className="text-main-color border-b ml-4 pb-2 mb-2 border-background-id font-semibold">
+                    Send Message
+                </a>
+                <div className="text-main-color border-b ml-4 pb-2 mb-2 border-background-id font-semibold">
+                    Share Contact
+                </div>
+                <div className="text-main-color ml-4 font-semibold">Add To Favorite</div>
+            </div>
+
+            <div className="flex flex-col p-3 rounded-xl bg-white mb-4 text-main-color text-md  font-semibold">
+                Add To Emergency Contact
+            </div>
+
+            <div className="flex flex-col p-3 rounded-xl bg-white mb-4 text-main-color text-md  font-semibold">
+                Share My Location
+            </div>
         </div>
+
     )
 }
 
